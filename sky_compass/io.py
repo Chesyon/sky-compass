cfb0d546d55a74336ca3bc5897e9f5d1e1c22843
Chesyon <55322011+Chesyon@users.noreply.github.com>
2026-05-21 13:48:46 -0400

    Create io.py

---
+++ sky_compass/io.py
@@ -0,0 +1,56 @@
+# Copyright 2026 Chesyon, under the MIT License
+# This file is responsible for communicating between the "frontend" and "backend".
+
+import json
+from section_selection import section_for_offset, Issue
+from mapper import MappingResult, Region
+
+
+def region_from_str(string: str) -> Region | None:
+    match string:
+        case "na":
+            return Region.na
+        case "eu":
+            return Region.eu
+        case "jp":
+            return Region.jp
+        case _:
+            return None
+
+# Put a json in, get a json out.
+def json_io(s : str) -> str:
+  deserialized = json.loads(s)
+  output_dat = deserialized_io(deserialized)
+  # TODO: Add a short name field to GlobalSection that matches the expected json name. __str__ will use that, and the current one can become a CLI specific function.
+  if "sections" in output_dat.keys():
+    output_dat.update({"sections": [str(section) for section in output_dat["sections"]]}) # dumps doesn't have a way to map GlobalSection, so we map it ourselves
+  return json.dumps(output_dat)
+
+def mapping_result_to_dict(result: MappingResult):
+  # TODO: Do we NEED this succeeded field? We could get away with using "result" (result is not None) as a conditional.
+  return {"succeeded": result.succeeded, "result": result.result, "min": result.min, "max": result.max}
+
+# TODO: Write typehints for raw
+# TODO: Can raw use Mapping?
+def deserialized_io(raw, skip_verification : bool = False):
+  output = {"response": "Invalid input"}
+  if not skip_verification:
+    if not isinstance(raw, dict): return output # raw is not a dict (huh?)
+    if raw.keys() != ['address', 'region', 'section']: return output
+    if not isinstance(raw["address"], int): return output
+    if not isinstance(raw["region"], str): return output
+    if not isinstance(raw["section"], str): return output
+  address = raw["address"]
+  region = region_from_str(raw["region"])
+  if not skip_verification:
+    if region is None: return output
+  sections, section_issues = section_for_offset(address, region, raw["section"])
+  output.update({"sections": sections, "issues": section_issues})
+  if len(sections) != 1 or Issue.INVALID_SECTION in section_issues:
+    output.update({"response": "Section error"}) # A section issue prevents mapping from continuing. (This is normal; an expected use of sky-compass is just to check the section of an address.)
+    return output
+  # We're good to map!
+  # TODO: Move handling of is_ov36 into GlobalSection.map_offset; there's really no reason the IO/CLI needs to handle this itself. I've already added the param here, but it needs to be added to mapper.py.
+  na_result, eu_result, jp_result = sections[0].map_offset(address, region, Issue.IS_OV36 in issues)
+  output.update({"response": "Success", "outputs": {"na": mapping_result_to_dict(na_result), "eu": mapping_result_to_dict(eu_result), "jp": mapping_result(jp_result)}})
+  return output
