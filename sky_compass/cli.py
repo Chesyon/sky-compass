0fc8557b9a593dd664afd413f90615fcf5020c3a
Chesyon <55322011+Chesyon@users.noreply.github.com>
2026-05-22 11:51:11 -0400

    Use IO in CLI

--- sky_compass/cli.py
+++ sky_compass/cli.py
@@ -2,21 +2,9 @@
 # The 'frontend' for sky-compass.
 
 import argparse
+from io import deserialized_io
 from section_selection import section_for_offset, Issue
 from mapper import Region
-
-
-def region_from_str(string: str) -> Region | None:
-    match string.lower():
-        case "na":
-            return Region.na
-        case "eu":
-            return Region.eu
-        case "jp":
-            return Region.jp
-        case _:
-            return None
-
 
 def region_name(region: Region) -> str:
     match region:
@@ -27,6 +15,22 @@ def region_name(region: Region) -> str:
         case Region.jp:
             return "JP"
 
+# TODO: This probably makes MappingResult.__str__ redundant. IO doesn't even return the MappingResult, it returns a dict, so we can't even *use* that function for the CLI anymore.
+def offset_result_string(result_dict) -> str:
+    result = result_dict["result"]
+    if result:
+        return hex(result)
+    else:
+        min = result_dict["min"]
+        max = result_dict["max"]
+        if not min: # if min is None
+            if not max:
+                return "Both nearest symbols were missing for this region, so no information can be given"
+            return f"The nearest lesser symbol was missing for this region, but the address should be no greater than {hex(self.max)}"
+        if not max:
+            return f"The nearest greater symbol was missing for this region, but the address should be no less than {hex(self.min)}"
+        return f"Could not find exact offset. Should be between {hex(self.min)} and {hex(self.max)}"
+
 def main() -> None:
     parser = argparse.ArgumentParser(
         prog="sky-compass",
@@ -34,43 +38,43 @@ def main() -> None:
     )
     parser.add_argument("region", choices=['na', 'eu', 'jp'], type=lambda reg: "na" if reg.lower() == "us" else reg.lower(), help="Source region of address")
     parser.add_argument("address", type=lambda addr: int(addr, 16), help="Address to be converted")
-    parser.add_argument("section", type=lambda sec: sec.lower().replace("overlay", "ov").replace("_",""), required=False, help="What overlay/section the address is in. If blank, sky-compass will try to figure it out on its own.") # TODO: add choices
+    parser.add_argument("section", type=lambda sec: sec.lower().replace("overlay", "ov").replace("_",""), required=False, help="What overlay/section the address is in. If blank, sky-compass will try to figure it out on its own.") # TODO: add choices ([str(sec) for sec in sections]? this doesn't include sub-sections though)
     args = parser.parse_args()
-    # Region
-    region = region_from_str(args.region)
-    # Section
-    sections, section_issues = section_for_offset(args.address, region, args.section)
-    for issue in section_issues:
+    output = deserialized_io({"address": args.address, "region": args.region, "section": args.section}, True)
+    sections = output["sections"]
+    # TODO: Add a cli_str function to the Issue class, have the error messages defined there. MULTIPLE_VALID_SECTIONS will probably still need to have some special behavior though.
+    for issue in output["issues"]:
         match issue:
             case Issue.VERIFICATION_FAILED:
                 print("Offset isn't in the provided section.")
             case Issue.INVALID_SECTION:
+                # TODO: This shouldn't be possible IF we add choices to the section arg. If this were fatal, I'd remove this case and let argparse handle it, but section selection is specifically DESIGNED to handle invalid sections, so argparse doesn't even need to block this? I might just not need choices at all... still would like a way to tell the user what's considered valid, though.
                 print("Provided section does not exist.")
             case Issue.FINDING_AUTOMATICALLY:
                 print("Finding section automatically...")
             case Issue.NO_VALID_SECTIONS:
                 print("Offset was not found in any possible section.")
-                exit(1)
             case Issue.MULTIPLE_VALID_SECTIONS:
                 num_sections = len(sections)
                 possible_sections_str = f"{num_sections} sections contain this offset: "
                 for i in range(num_sections):
                     if i == num_sections - 1:
                         possible_sections_str += "and "
-                    possible_sections_str += sections[i].name
+                    possible_sections_str += sections[i].cli_str()
                     if i != num_sections - 1:
                         possible_sections_str += ", "
                     else:
                         possible_sections_str += "."
                 print(possible_sections_str)
                 print("Please retry, providing the desired section.")
-                exit(1)
-    # Map
-    section = sections[0]
-    na_offset, eu_offset, jp_offset = section.map_offset(args.offset, region)
+    # I could just put exits in the the issues that would stop mapping above, but this is probably better for readability.
+    # TODO: Not a huge fan of needing to have the "Success" string defined in two separate places. Either make this an enum in the same way as Issue, or make a constant for this.
+    if output["response"] != "Success":
+        exit(1)
     # Display output offsets.
+    outputs = output["outputs"]
     print(
-        f"{hex(args.offset)} [{region_name(region)}] in {section.cli_str()}:\nNA: {na_offset}\nEU: {eu_offset}\nJP: {jp_offset}"
+        f"{hex(args.offset)} [{args.region.upper()}] in {sections[0]}:\nNA: {offset_result_string(outputs["na"])}\nEU: {offset_result_string(outputs["eu"])}\nJP: {offset_result_string(outputs["jp"])}"
     )
 
 if __name__ == "__main__":
